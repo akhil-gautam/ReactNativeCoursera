621f1ce397c23a4d8bd1555944494e84c8c3dfe2
akhil-gautam <akhilg@mindfiresolutions.com>
2018-11-07 22:08:30 +0530

    added components for listing

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Main from './components/MainComponent';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
+
---
+++ components/MenuComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+const Menu = (props) => {
+
+  const renderMenuItem = ({item, index}) => {
+    return(
+      <ListItem
+        key={index}
+        title={item.name}
+        subtitle={item.description}
+        hideChevron={true}
+        leftAvatar={{source: require('./images/uthappizza.png')}}
+      />
+    )
+  }
+  
+  return(
+    <FlatList
+      data={props.dishes}
+      renderItem={renderMenuItem}
+      keyExtractor={item => item.id.toString()}
+    />
+  )
+  
+}
+
+export default Menu;
