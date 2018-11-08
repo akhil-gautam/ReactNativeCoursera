a4ede660612ccdcb6a631fe276ab98c715f5bb9c
akhil-gautam <akhilg@mindfiresolutions.com>
2018-11-09 00:48:39 +0530

    added trending to homepage

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+const RenderItem = (props) => {
+  const {item} = props;
+  if(item != null){
+    return(
+      <Card
+        featuredTitle={item.name}
+        featuredSubtitle={item.designation}
+        image={require('./images/uthappizza.png')}
+      >
+        <Text style={{margin: 10}}>
+          {item.description}
+        </Text>
+      </Card>
+    )
+  } else {
+    return(
+      <View></View>
+    )
+  }
+}
 
 class Home extends Component{
-  
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+    }
+  }
   static navigationOptions = {
     title: 'Home'
   }
   render(){
     return(
-      <View style={{flex: 1, alignContent: "center"}}>
-        <Text>
-          This is the home screen.
-        </Text>
-      </View>
+      <ScrollView>
+        <RenderItem
+          item={this.state.dishes.filter((dish) => dish.featured)[0]}
+        />
+        <RenderItem
+          item={this.state.promotions.filter((promo) => promo.featured)[0]}
+        />
+        <RenderItem
+          item={this.state.leaders.filter((lead) => lead.featured)[0]}
+        />
+      </ScrollView>
     )
   }
 }
